37d22a2f4f36be9641795a026608196ef3071cf3
Marko Samastur <markos@gaivo.net>
2016-10-18 22:49:15 +0200

    Added optimization of static files

---
+++ shirkers/assets.py
@@ -0,0 +1,19 @@
+from django_assets import Bundle, register
+
+css_files = [
+    'static/jquery-ui.min.css',
+    'static/pure/base-min.css',
+    'static/pure/forms-min.css',
+    'static/pure/buttons-min.css',
+]
+
+js_files = [
+    'static/jquery.js',
+    'static/jquery-ui.min.js',
+]
+
+js = Bundle(*js_files, filters='jsmin', output='static/scripts.js')
+css = Bundle(*css_files, filters='cssmin', output='static/styles.css')
+
+register('js_all', js)
+register('styles_all', css)
